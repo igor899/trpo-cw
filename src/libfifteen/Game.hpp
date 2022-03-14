57f14deacbe195a7c1141b6c5b1dfa836a42ce2c
Igor Degtyarev <degterev1738@gmail.com>
2022-03-14 23:55:17 +0700

    add constants.hpp

--- src/libfifteen/Game.hpp
+++ src/libfifteen/Game.hpp
@@ -8,12 +8,7 @@
 #include <random>
 #include <string>
 
-#define IMG_PATH "src/libfifteen/img/"
-#define FONT_NAME "Roboto-Regular.ttf"
-#define GAME_WIDTH 600
-#define GAME_HEIGHT 400
-#define GAME_NAME "Fifteen Game"
-#define FILENAME_STAT "stat.bin"
+#include <libfifteen/constants.hpp>
 
 class Game {
 protected:
---
+++ src/libfifteen/constants.hpp
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+#define IMG_PATH "src/libfifteen/img/"
+#define FONT_NAME "Roboto-Regular.ttf"
+#define GAME_WIDTH 600
+#define GAME_HEIGHT 400
+#define GAME_NAME "Fifteen Game"
+#define FILENAME_STAT "stat.bin"
+
+#endif // #ifndef CONSTANTS_HPP
